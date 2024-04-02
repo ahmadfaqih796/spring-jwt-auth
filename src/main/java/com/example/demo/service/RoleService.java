f8c1ac7672fcf70cc06c9e53004506f7c7e27523
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-04-02 19:07:41 +0700

    role service create

---
+++ src/main/java/com/example/demo/service/RoleService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.entity.RoleEntity;
+import com.example.demo.model.repository.RoleRepository;
+
+import jakarta.transaction.TransactionScoped;
+
+@Service
+@TransactionScoped
+public class RoleService {
+
+   @Autowired
+   private RoleRepository roleRepository;
+
+   public RoleEntity create(RoleEntity role) {
+      return roleRepository.save(role);
+   }
+
+}
