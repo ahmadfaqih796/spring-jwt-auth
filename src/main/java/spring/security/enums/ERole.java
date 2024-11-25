065e70981551b5875e9cb37be68392f6060afeba
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-11-25 09:18:09 +0700

    role entity

---
+++ src/main/java/spring/security/enums/ERole.java
@@ -0,0 +1,7 @@
+package spring.security.enums;
+
+public enum ERole {
+  ROLE_USER,
+  ROLE_MODERATOR,
+  ROLE_ADMIN,
+}
---
+++ src/main/java/spring/security/models/Role.java
@@ -0,0 +1,44 @@
+package spring.security.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import spring.security.enums.ERole;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "name", length = 20)
+  private ERole name;
+
+  public Role() {}
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public ERole getName() {
+    return name;
+  }
+
+  public void setName(ERole name) {
+    this.name = name;
+  }
+  // Getters dan Setters
+
+}
